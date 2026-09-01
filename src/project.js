f21f3155b57b674509aac97c7086416cb2ad65ed
nickxhan <nickxhan@umich.edu>
2026-09-01 16:07:29 -0400

    Add static method to Project

--- src/project.js
+++ src/project.js
@@ -7,6 +7,12 @@ class Project {
         this.name = name;
     }
 
+    //project 1 has the todo to remove, project2 doesn't
+    static transferTodo(project1, project2, todo) {
+        project2.addTodo(todo);
+        project1.removeTodo(todo);
+    }
+
     removeTodo(todo) {
         this.#todos.filter(t => t.id !== todo.id);
     }
