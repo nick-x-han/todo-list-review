ed80bf5ba5710579ec9b88de0e968a2e3854980d
nickxhan <nickxhan@umich.edu>
2026-09-02 23:45:36 -0400

    Change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-git checkout gh-pages &&
-git merge main --no-edit &&
 npm run build &&
-git add dist -f &&
-git commit -m "Deployment commit" &&
-npm run deploy &&
-git checkout main
-
-#bash deploy.sh, ./deploy.sh, chmod +x deploy.sh
+npm run deploy
---
+++ deploy_old.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git checkout gh-pages &&
+
+git merge main --no-edit &&
+
+npm run build &&
+
+git add dist -f &&
+
+git commit -m "Deployment commit" &&
+
+npm run deploy &&
+
+git checkout main
+
+#bash deploy.sh, ./deploy.sh, chmod +x deploy.sh
